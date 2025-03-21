65257bcbe6ed79fe4685299b128168f06e645a8e
robertsbaer <robertsbaer@yahoo.com>
2025-03-21 03:14:23 -0400

    seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',  // Change this from '/portfolio/' to '/'
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 80
+      },
+      png: {
+        quality: 80
+      },
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                removeViewBox: false,
+                removeTitle: false,
+              },
+            },
+          },
+        ],
+      }
+    })
+  ]
 })
